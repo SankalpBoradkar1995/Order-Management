1ad2b0a163bbc2bc31645f9c303788b96e02f7af
Sankalp <boradkarsankalp6@gmail.com>
2024-11-15 13:46:07 +0530

    Added versioning in parent pom and spring boot class in Process-Orders module

---
+++ Process-Orders/src/main/java/com/process/orders/ProcessOrders.java
@@ -0,0 +1,14 @@
+package com.process.orders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessOrders {
+
+	public static void main(String[] args) {
+		 SpringApplication.run(ProcessOrders.class, args);
+
+	}
+
+}
